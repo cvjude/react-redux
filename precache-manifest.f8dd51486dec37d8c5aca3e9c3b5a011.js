e550c6f7bf51e3527fe52a92ed0661d0c754bd8d
cvjude <jjchinosoviolet@gmail.com>
2019-07-19 13:32:21 +0100

    Updates

---
+++ precache-manifest.f8dd51486dec37d8c5aca3e9c3b5a011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae92baebad6064a2931a1bc81e7fca4a",
+    "url": "/react-redux/index.html"
+  },
+  {
+    "revision": "9b739b9ea92bd3ad079f",
+    "url": "/react-redux/static/css/main.6b6a4eb6.chunk.css"
+  },
+  {
+    "revision": "9796e99321789d4327e2",
+    "url": "/react-redux/static/js/2.67770484.chunk.js"
+  },
+  {
+    "revision": "9b739b9ea92bd3ad079f",
+    "url": "/react-redux/static/js/main.8c2624ff.chunk.js"
+  },
+  {
+    "revision": "d992f6dd91a79b7f0683",
+    "url": "/react-redux/static/js/runtime~main.e2ec0b4c.js"
+  },
+  {
+    "revision": "fd721f81cf12ebf7b3234c2c665b584c",
+    "url": "/react-redux/static/media/pokeball.fd721f81.png"
+  }
+]);
